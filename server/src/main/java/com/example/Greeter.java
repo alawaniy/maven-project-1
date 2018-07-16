fa3e676e833874fb4f959e8960c5c2373648501d
Ankit Lawaniya <ankitlawaniya007@gmail.com>
2018-07-16 19:12:42 +0530

    Jai mata di saab

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,8 @@ public class Greeter {
   }
   
  /**
- * This is a class.
+ * @param someone is name of the person
+ * @return greeting string
  */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -1,6 +1,6 @@
 /**
 *
-This is the Package information
+This is the Package name.
 *
 */
 package com.example;
