90354f63b8e047222de4f166ef181185e6fa48ce
Ankit Lawaniya <ankitlawaniya007@gmail.com>
2018-07-16 18:23:50 +0530

    Thi is Mumbai

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,13 @@ public class Greeter {
   public Greeter() {
 
   }
-
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+ 
+ /*
+ This is Java doc ocmments
+ 
+ */
+ 
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
